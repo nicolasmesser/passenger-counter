a1ab59a5e8d298f4e21eaba727941ffbe8633143
Nicolas Messer <nme@nicolasmesser.ch>
2024-05-24 16:24:54 +0200

    Improving Saving of Previous Entries

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,27 @@
 let count = 0
+let countArr = []
+let countStr = ""
 let countEl = document.getElementById('count-el')
 let saveEl = document.getElementById('save-el')
 
 function increment() {
-  count += 1
-  countEl.textContent = count
+  count += 1;
+  countEl.textContent = count;
 }
 
 function save() {
-  let countStr = count + " - "
-  saveEl.textContent += countStr
-  count = 0
-  countEl.textContent = count
+  countStr = "";
+  countArr.push(count);
+  if (countArr.length === 1) {
+    countStr += countArr[0];
+  } else {
+    for (let i = 0; i < countArr.length-1; i++) {
+      countStr += countArr[i] + " - ";
+    }
+    countStr += countArr[countArr.length-1];
+  }
+  saveEl.textContent = "";
+  saveEl.textContent += countStr;
+  count = 0;
+  countEl.textContent = count;
 }
