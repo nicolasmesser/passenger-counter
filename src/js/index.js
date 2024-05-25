9cdc0688b9d7ab38dd9d85fea22b4826d21ecb80
Nicolas Messer <nme@nicolasmesser.ch>
2024-05-25 12:52:32 +0200

    fix(app): Fixing Reset Button Issue
    Array with saved values was not emptied during reset.

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,8 @@
-let count = 0
-let countArr = []
-let countStr = ""
-let countEl = document.getElementById('count-el')
-let saveEl = document.getElementById('save-el')
+let count = 0;
+let countArr = [];
+let countStr = "";
+let countEl = document.getElementById('count-el');
+let saveEl = document.getElementById('save-el');
 
 function increment() {
   count += 1;
@@ -27,5 +27,6 @@ function save() {
 }
 
 function reset() {
+  countArr = [];
   saveEl.textContent = "";
 }
